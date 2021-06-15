f5851f04cc2299f2d1f1dd380111dc37a0204659
ios_dev <futureiosdeveloper@gmail.com>
2021-06-15 09:26:46 +0300

    DONE thursday

--- randomizer2021june/SettingsViewController.swift
+++ randomizer2021june/SettingsViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
 
+class SettingsViewController: UIViewController  {
+    
     @IBOutlet var minimumTF: UITextField!
     @IBOutlet var maximumTF: UITextField!
     
     var minimumValue: String!
     var maximumValue: String!
+    
+    var minValueSettingsVC = Int()
+    var maxValueSettingsVC = Int()
+    let maxValue = 101
+    
+    let letters = NSCharacterSet.letters
     
     override func viewDidLoad() {
         super .viewDidLoad()
@@ -21,27 +28,35 @@ class SettingsViewController: UIViewController {
         maximumTF.text = maximumValue
     }
     
-    
-    
-    
-    @IBAction func saveButtonPressed() {
-
- 
-        
-        
+    @IBAction func saveButtonPressed(_ sender: AnyObject) {
+        tfIntoInt ()
     }
-    
-    
-
     
     @IBAction func cancelAction() {
-        
         dismiss(animated: true)
+        // performSegue(withIdentifier: "unwindSegueIdent", sender: nil)
     }
     
- 
+    private  func tfIntoInt () {
+        let minTF = Int(minimumTF.text ?? "")
+        let maxTF = Int(maximumTF.text ?? "")
+        if minTF! >= maxTF! || minTF! >= maxValue || maxTF! >= maxValue  {
+            showAlert(with: "Wrong format", and: "Min can't be more max. Min = 0, max = 100")
+        } else {
+            return
+                performSegue(withIdentifier: "unwindSegueThuesday", sender: nil)
+            // сегуэ создаем путем перетаскивания линии в панели ДРЕВЕ элементов
+            // даем сегуэ идентификатор
+        }
+    }
+}
 
-    
-    
-    
+extension SettingsViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style:  .default) { _ in
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
--- randomizer2021june/ViewController.swift
+++ randomizer2021june/ViewController.swift
@@ -8,87 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet var zeroLabel: UILabel!
     @IBOutlet var maxLabel: UILabel!
-    
     @IBOutlet var resultLabel: UILabel!
     
     @IBOutlet var resultButton: UIButton!
-    
     @IBOutlet var randomRangeButton: UIButton!
     
-    private let arrayNumbers1 = 0...50
-    private let arrayNumbers2 = 50...100
+    private let arrayNumbers = 0...100
     
-    var min = Int()
-    var max = Int()
-    
+    private  var min = 0
+    private  var max: Int = 0
+    private  let maxValue = 101
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        randomNumberIntoRandomButton()
     }
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is SettingsViewController {
             guard let settingsVC = segue.destination as? SettingsViewController  else { return }
             settingsVC.minimumValue = zeroLabel.text
             settingsVC.maximumValue = maxLabel.text
-   
+            // передаем значения из лейблов в текстфилд
+        }
     }
-    }
-    
-    
-    
-    
-    
     
     @IBAction func randomRangeButtonPressed() {
-        
-        minAndMax()
-        zeroLabel.text = String(min)
-        maxLabel.text = String(max)
+        randomNumberIntoRandomButton()
     }
     
-    @IBAction func getResultPressed() {
-
-        resultLabel.text = String(Int.random(in: min ... max))
-
+    @IBAction func getResultPressed(_ sender: UIButton) {
+        //resultLabel.text = String(Int.random(in: min ... max))
+        // нажатие кнопки GetResult
+        getRandomFromTF()
+        
     }
     
-   
-    private func minAndMax() {
+    func randomNumberIntoRandomButton() {
+        if min <= 99 {
+            max = Int.random(in: min...100)
+            zeroLabel.text = String(min)
+            maxLabel.text = String(max)
+            // делаем для кнопки рандом метод
+        }
+    }
+    
+    private func getRandomFromTF() {
+        guard let zeroInt = Int(zeroLabel.text ?? "error") else { return }
+        guard let maxInt = Int(maxLabel.text ?? "error") else { return }
         
-        min = Int(arrayNumbers1.randomElement() ?? 000)
-        max = Int(arrayNumbers2.randomElement() ?? 0000)
+        let randomInt = Int.random(in: zeroInt..<maxInt)
+        
+        resultLabel.text = String(randomInt)
+        
+        // конвертируем лейбл в Int
+        // вызываем рандомно метод из закрытого диапазона
     }
     
     @IBAction func unwindSegue (segue: UIStoryboardSegue) {
+        
+        // со второго VC мы передае на этот
         
         guard let settingsVC = segue.source as? SettingsViewController else {
             return
         }
         
-        zeroLabel.text = settingsVC.minimumTF.text
-        maxLabel.text =  settingsVC.maximumTF.text
-
+        settingsVC.maxValueSettingsVC = Int(settingsVC.maximumTF.text ?? "") ?? 0
+        settingsVC.minValueSettingsVC = Int(settingsVC.minimumTF.text ?? "") ?? 0
         
+        //из текст филда получаем Int
+        
+        zeroLabel.text = String(settingsVC.minValueSettingsVC)
+        maxLabel.text = String(settingsVC.maxValueSettingsVC)
+        
+        //передаем с TF в лейблы на первый экран
     }
     
- 
- 
-    
-    
-    
 }
-
-
-
-
-    
-
-
-
